5eb90693413e273122f4c6e03c7dd75c40a9c05c
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-09-13 01:10:26 -0700

    add

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,12 +110,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-if debug==True :
- STATIC_ROOT=BASE_DIR/'static'
-else:
-    STATICFILES_DIRS=[
-    BASE_DIR/'static'
-    ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT=BASE_DIR/'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
