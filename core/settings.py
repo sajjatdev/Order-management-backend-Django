5876c4625b16c5b0a797910ed21d9d2f4c4ac093
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-09-13 01:04:12 -0700

    add

--- core/settings.py
+++ core/settings.py
@@ -7,19 +7,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(id#t0w8q@r5i3n$7wst2npvbvgd(8i53a(yhw@cfg3tmp82ja'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ["adminpizza.herokuapp.com",'127.0.0.1']
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'jazzmin',
